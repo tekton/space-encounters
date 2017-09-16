2f17dd6a25896ae8412f18edab10da084c28c1ad
Adamcroman <adam@imminentdigital.com>
2017-09-15 23:16:42 -0700

    Starting out

---
+++ generator.py
@@ -0,0 +1,6 @@
+import random
+
+ENCOUNTERTYPES = ("Dead", "Volcanic", "Oceanic", "Jungle", "Asteroid Field", "wormhole", "Space Station", "Ship", "Gas Giant", "Temperate World", "Ice Planet", "Space Hulk")
+encountertype = random.choice(ENCOUNTERTYPES)
+
+print encountertype
