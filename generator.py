10e2c59a70ea58fa607d883043d7cebf5cc20e48
Adamcroman <adam@imminentdigital.com>
2017-09-15 23:32:41 -0700

    Attempting Dictionary

--- generator.py
+++ generator.py
@@ -1,6 +1,19 @@
 import random
 
-ENCOUNTERTYPES = ("Dead", "Volcanic", "Oceanic", "Jungle", "Asteroid Field", "wormhole", "Space Station", "Ship", "Gas Giant", "Temperate World", "Ice Planet", "Space Hulk")
+ENCOUNTERTYPES = {
+	"Dead": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"}, 
+	"Volcanic": {"Population": "VolcanicWorldPop", "Atmosphere": "VolcanicWorldAtmo", "DayLen": "VolcanicworldDayLen", "Year": "VolcanicWorldYear"}, 
+	"Oceanic": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"},
+	"Jungle": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"},
+	"Asteroid Field": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"},
+	"wormhole": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"}, 
+	"Space Station": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"},
+	"Ship": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"},
+	"Gas Giant": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"},
+	"Temperate World": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"},
+	"Ice Planet": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"},
+	 "Space Hulk": {"Population": "DeadWorldPop", "Atmosphere": "DeadWorldAtmo", "DayLen": "DeadworldDayLen", "Year": "DeadworldYear"}
+}
 encountertype = random.choice(ENCOUNTERTYPES)
 
 print encountertype
