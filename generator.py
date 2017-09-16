15614aca05e45740f464b17ed05786e414d3fd09
Adamcroman <adam@imminentdigital.com>
2017-09-16 00:31:29 -0700

    Finishing for the night

--- generator.py
+++ generator.py
@@ -18,7 +18,7 @@ MedWorldPop = random.choice(MEDWORLDPOPGEN)
 HIGHWORLDPOPGEN = (  
 	"500,001-1,000,000",
 	"1MM-20MM",
-	"21MM-100MM"
+	"21MM-100MM",
 	"100MM-1B",
 	"1B-8B"
 	)
@@ -48,7 +48,7 @@ ShipAtmo = random.choice(SHIPATMOGEN)
 
 WORMHOLESIZEGEN = (
 	"Tiny",
-	"Small"
+	"Small",
 	"Medium",
 	"Large",
 	"Huge",
@@ -62,15 +62,15 @@ ENCOUNTERTYPES = {
 	"Volcanic": {"Type": "Volcanic World", "Population": LowWorldPop, "Atmosphere": WorldAtmo, "DayLen": "VolcanicworldDayLen", "Year": "VolcanicWorldYear", "Resources": "VolcanicResources"}, 
 	"Oceanic": {"Type": "Oceanic World", "Population": MedWorldPop, "Atmosphere": WorldAtmo, "DayLen": "OceanicDayLen", "Year": "OceanicworldYear"},
 	"Jungle": {"Type": "Jungle World", "Population": MedWorldPop, "Atmosphere": LivingWorldAtmo, "DayLen": "JungleworldDayLen", "Year": "JungleworldYear", "Resources": "JungleResources"},
-	"Asteroid Field": {"Type": "Asteroid Field", "Population": LowWorldPop, "Atmosphere": "AsteroidAtmo", "Resources": "AsteroidResources"},
-	"Wormhole": {"Type": "WormHole", "Size": "WormholeSize", "Stability": "WormHoleStab"}, 
-	"Space Station": {"Type": "Space Station", "Population": LowWorldPop, "Atmosphere": "StationAtmo", "DayLen": "StationDayLen", "Year": "StationYear"},
+	"Asteroid Field": {"Type": "Asteroid Field", "Population": LowWorldPop, "Atmosphere": WorldAtmo, "Resources": "AsteroidResources"},
+	"Wormhole": {"Type": "WormHole", "Size": WormholeSize, "Stability": "WormHoleStab"}, 
+	"Space Station": {"Type": "Space Station", "Population": LowWorldPop, "Atmosphere": ShipAtmo, "DayLen": "StationDayLen", "Year": "StationYear"},
 	"Ship": {"Type": "Ship", "Population": LowWorldPop, "Atmosphere": WorldAtmo, "Condition": "ShipCondition", "Disposition": "ShipDisposition"},
-	"Gas Giant": {"Type": "Gas Giant", "Population": LowWorldPop, "Atmosphere": "GasAtmo", "DayLen": "GasDayLen", "Year": "GasYear", "Resources": "GasResources"},
+	"Gas Giant": {"Type": "Gas Giant", "Population": LowWorldPop, "Atmosphere": WorldAtmo, "DayLen": "GasDayLen", "Year": "GasYear", "Resources": "GasResources"},
 	"Temperate World": {"Type": "Temperate World", "Population": MedWorldPop, "Atmosphere": LivingWorldAtmo, "DayLen": "TemperateDayLen", "Year": "TemperateYear", "Resources": "TemperateResources"},
 	"Ice Planet": {"Type": "Ice World", "Population": LowWorldPop, "Atmosphere": WorldAtmo, "DayLen": "IceworldDayLen", "Year": "IceworldYear"},
 	"Space Hulk": {"Type": "Space Hulk", "Population": LowWorldPop, "Atmosphere": WorldAtmo, "Hostiles": "SpaceHulkHostiles"}
-},
+}
 
 encounter = ENCOUNTERTYPES[random.choice(ENCOUNTERTYPES.keys())]
 
