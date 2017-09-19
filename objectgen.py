bd78f1fbfd88ea7e3ce3cfedd568ee36c5ec2cb0
Adamcroman <adam9184@gmail.com>
2017-09-19 14:31:59 -0700

    fixing errors

--- objectgen.py
+++ objectgen.py
@@ -34,11 +34,11 @@ def encountergen():
 	med_world_pop = "{} life signs".format(med_world_pop_gen)
 	high_world_pop_gen = random.randint(500000, 8000000000)
 	high_world_pop = "{} life signs".format(high_world_pop_gen)
-	world_atmo = random.choice(ATMOGEN)
+	world_atmo = random.choice(atmogen)
 	living_world_atmo = random.choice(living_world_atmogen)
 	ship_atmo = random.choice(ship_atmo_gen)
 	worm_hole_size = random.choice(wormhole_size_gen)
-	worm_hole_stab_gen = random.randint(01, 100)
+	worm_hole_stab_gen = random.randint(1, 100)
 	worm_hole_stab = "{} Hours".format(worm_hole_stab_gen)
 	day_hours = random.randint(8, 96)
 	day_len = "{} Hours".format(day_hours)
@@ -64,13 +64,13 @@ def encountergen():
 	"Dead": {"Type": "Dead World", "Population": low_world_pop, "Atmosphere": world_atmo, "Day Length": day_len, "Year": year_len}, 
 	"Volcanic": {"Type": "Volcanic World", "Population": low_world_pop, "Atmosphere": world_atmo, "Day Length": day_len, "Year": year_len}, 
 	"Oceanic": {"Type": "Oceanic World", "Population": med_world_pop, "Atmosphere": world_atmo, "Day Length": day_len, "Year": year_len},
-	"Jungle": {"Type": "Jungle World", "Population": med_world_pop, "Atmosphere": Livingworld_atmo, "Day Length": day_len, "Year": year_len},
+	"Jungle": {"Type": "Jungle World", "Population": med_world_pop, "Atmosphere": living_world_atmo, "Day Length": day_len, "Year": year_len},
 	"Asteroid Field": {"Type": "Asteroid Field", "Population": low_world_pop, "Atmosphere": world_atmo},
-	"Wormhole": {"Type": "WormHole", "Size": WormholeSize, "Stability": WormHoleStab}, 
-	"Space Station": {"Type": "Space Station", "Population": low_world_pop, "Atmosphere": ShipAtmo, "Station Condition": Con, "Disposition": Disposition},
-	"Ship": {"Type": "Ship", "Population": low_world_pop, "Atmosphere": world_atmo, "Condition": Con, "Disposition": Disposition},
+	"Wormhole": {"Type": "WormHole", "Size": worm_hole_size, "Stability": worm_hole_stab}, 
+	"Space Station": {"Type": "Space Station", "Population": low_world_pop, "Atmosphere": ship_atmo, "Station Condition": con, "Disposition": disposition},
+	"Ship": {"Type": "Ship", "Population": low_world_pop, "Atmosphere": world_atmo, "Condition": con, "Disposition": disposition},
 	"Gas Giant": {"Type": "Gas Giant", "Population": low_world_pop, "Atmosphere": world_atmo, "Day Length": day_len, "Year": year_len},
-	"Temperate World": {"Type": "Temperate World", "Population": med_world_pop, "Atmosphere": Livingworld_atmo, "Day Length": day_len, "Year": year_len},
+	"Temperate World": {"Type": "Temperate World", "Population": med_world_pop, "Atmosphere": living_world_atmo, "Day Length": day_len, "Year": year_len},
 	"Ice Planet": {"Type": "Ice World", "Population": low_world_pop, "Atmosphere": world_atmo, "Day Length": day_len, "Year": year_len},
 	"Space Hulk": {"Type": "Space Hulk", "Population": low_world_pop, "Atmosphere": world_atmo}
 	}
