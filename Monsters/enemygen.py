58bbad891fd7454b1654fde79e5e36a925f01462
Adamcroman <adam9184@gmail.com>
2017-09-28 12:08:53 -0700

    updating monster gen to add saving throws

--- Monsters/enemygen.py
+++ Monsters/enemygen.py
@@ -3,13 +3,14 @@ from collections import OrderedDict
 import pprint
 import math
 import sys
-from monster_stats import *
+from monster_stats import saves, monsterstatgen
 
 
-
-def statgen(enemystats, level):
+def statgen(enemy, level):
 	rtn_stats = OrderedDict()
-	rtn_stats = 
+	rtn_stats = monsterstatgen(enemy)
+	rtn_saves = OrderedDict()
+	rtn_saves = saves(level)
 	strmod = int(math.floor((rtn_stats["STR"] -10)/2))
 	dexmod = int(math.floor((rtn_stats["DEX"] -10)/2))
 	conmod = int(math.floor((rtn_stats["CON"] -10)/2))
@@ -20,17 +21,20 @@ def statgen(enemystats, level):
 	conmodtotal = monlevel*conmod
 	hitdie = int(random.randint(1,12))
 	hitpoints = (hitdie*monlevel) + conmodtotal
+	reflex = rtn_saves[rtn_stats["reflex"]]
+	fort = rtn_saves[rtn_stats["fort"]]
+	will = rtn_saves[rtn_stats["will"]]
 	rtn_stats["HP"] = hitpoints
 	rtn_stats["MAT"] = strmod+monlevel
 	rtn_stats["RAT"] = dexmod+monlevel
-	rtn_stats["REFSAVE"] = dexmod + int(math.floor(monlevel/2))
-	rtn_stats["FORTSAVE"] = conmod + int(math.floor(monlevel/2)+1)
-	rtn_stats["WILLSAVE"] = wismod 
+	rtn_stats["REFSAVE"] = dexmod + reflex
+	rtn_stats["FORTSAVE"] = conmod + fort
+	rtn_stats["WILLSAVE"] = wismod + will
 	return rtn_stats
 
 if __name__ == "__main__":
-	enemytype = str(sys.argv[1])
+	enemy = str(sys.argv[1])
 	level = str(sys.argv[2])
-	stats = statgen(level)
+	stats = statgen(enemy, level)
 
 	pprint.pprint(stats)
