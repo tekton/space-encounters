6c7a30bc9b86c3fedfc397c5fc499286340f89b4
adamcroman <adam9184@gmail.com>
2017-10-08 21:58:21 -0700

    Adding ship shields

--- spacecombatgen.py
+++ spacecombatgen.py
@@ -246,7 +246,28 @@ shipsensors = {
 }
 
 shipshields = {
-	
+	"Basic Shields 1": {"Total SP": "10", "Regen rate": "1 per min", "PCU": 5, "Cost": 2},
+	"Basic Shields 2": {"Total SP": "20", "Regen rate": "1 per min", "PCU": 10, "Cost": 3},
+	"Basic Shields 3": {"Total SP": "30", "Regen rate": "1 per min", "PCU": 15, "Cost": 4},
+	"Basic Shields 4": {"Total SP": "40", "Regen rate": "1 per min", "PCU": 15, "Cost": 5},
+	"Light Shields 1": {"Total SP": "50", "Regen rate": "2 per min", "PCU": 20, "Cost": 6},
+	"Light Shields 2": {"Total SP": "60", "Regen rate": "2 per min", "PCU": 20, "Cost": 8},
+	"Light Shields 3": {"Total SP": "70", "Regen rate": "2 per min", "PCU": 25, "Cost": 10},
+	"Light Shields 4": {"Total SP": "80", "Regen rate": "2 per min", "PCU": 30, "Cost": 12},
+	"Medium Shields 1": {"Total SP": "90", "Regen rate": "4 per min", "PCU": 30, "Cost": 13},
+	"Medium Shields 2": {"Total SP": "100", "Regen rate": "4 per min", "PCU": 30, "Cost": 15},
+	"Medium Shields 3": {"Total SP": "120", "Regen rate": "4 per min", "PCU": 35, "Cost": 17},
+	"Medium Shields 4": {"Total SP": "140", "Regen rate": "8 per min", "PCU": 40, "Cost": 18},
+	"Medium Shields 5": {"Total SP": "160", "Regen rate": "8 per min", "PCU": 45, "Cost": 20},
+	"Medium Shields 6": {"Total SP": "200", "Regen rate": "8 per min", "PCU": 50, "Cost": 22},
+	"Heavy Shields 1": {"Total SP": "240", "Regen rate": "16 per min", "PCU": 55, "Cost": 23},
+	"Heavy Shields 2": {"Total SP": "280", "Regen rate": "16 per min", "PCU": 60, "Cost": 25},
+	"Heavy Shields 3": {"Total SP": "320", "Regen rate": "16 per min", "PCU": 70, "Cost": 27},
+	"Heavy Shields 4": {"Total SP": "360", "Regen rate": "32 per min", "PCU": 80, "Cost": 28},
+	"Heavy Shields 5": {"Total SP": "420", "Regen rate": "32 per min", "PCU": 90, "Cost": 30},
+	"Heavy Shields 6": {"Total SP": "480", "Regen rate": "32 per min", "PCU": 110, "Cost": 32},
+	"Superior Shields 1": {"Total SP": "540", "Regen rate": "64 per min", "PCU": 130, "Cost": 35},
+	"Superior Shields 2": {"Total SP": "600", "Regen rate": "64 per min", "PCU": 160, "Cost": 40}
 }
 
 shipweapons = {
